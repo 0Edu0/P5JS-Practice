2f807c81497996acd908e3d43396ce0c7809491d
Eduardo <edulink.1610@gmail.com>
2026-04-06 20:09:17 -0600

    Completed exercise for conditional statements

--- Conditional_Statements/sketch.js
+++ Conditional_Statements/sketch.js
@@ -1,7 +1,25 @@
 function setup(){
-
+    createCanvas(600, 400);
 }
 
 function draw(){
-    
+    background(0);
+
+    if(mouseX <= 200){
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(300, 200, 100);
+    } else if(mouseX <= 400){
+        rectMode(CENTER);
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        rect(300, 200, 100, 100);
+    } else if(mouseX <= 600){
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        triangle(250, 250, 300, 150, 350, 250);
+    }
 }
